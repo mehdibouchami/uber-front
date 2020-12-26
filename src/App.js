956f34964fe2a10da71fa7a977742eae16ecd251
mehdibouchami <mehdibouchami77@gmail.com>
2020-12-26 13:34:59 +0100

    test components

---
+++ src/App.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react'
+import { Menu } from 'semantic-ui-react'
+import Home from "./components/home";
+import Users from "./components/users";
+import Map from "./components/map";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+//const colorsA = ['red', 'orange', 'yellow', 'olive', 'green', 'teal']
+
+
+export default class MenuExampleColoredInverted extends Component {
+  state = { active: 'Home'}
+
+  handleClick = (e, { name }) => this.setState({ active: name })
+  
+
+  render() {
+    const { active } = this.state
+
+    return (
+      <div>
+        <Router>
+        <Menu inverted>
+        <Menu.Item
+              key='home'
+              name='Home'
+              active={active === 'Home'}
+              color='blue'
+              as={Link} 
+              to='/'
+              onClick={this.handleClick}
+            />
+            <Menu.Item
+              key='user'
+              name='User'
+              active={active === 'User'}
+              color='red'
+              as={Link} 
+              to='/user'
+              onClick={this.handleClick}
+            />
+            <Menu.Item
+              key='map'
+              name='Map'
+              active={active === 'Map'}
+              color='yellow'
+              as={Link} 
+              to='/map'
+              onClick={this.handleClick}
+            />
+        </Menu>
+        <Switch>
+          <Route path="/map">
+            <Map />
+          </Route>
+          <Route path="/user">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+        </Router>
+      </div>
+    )
+  }
+}
---
+++ src/components/map.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Map() {
+    return (
+        <div>
+            <h2>Map</h2>
+        </div>
+    )
+}
